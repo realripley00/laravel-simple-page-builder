0e3feda81e3900b9f8610646325e52382edd23c3
Michael Burgin <realripley00@gmail.com>
2018-09-17 16:46:25 -0400

    adding paragraphs and images.

--- migrations/2018_09_17_0000000_create_buildable_tables.php
+++ migrations/2018_09_17_0000000_create_buildable_tables.php
@@ -6,17 +6,17 @@ class CreateBuildableTables extends Migration
 {
     public function up()
     {
-        Schema::table('buildingblocks', function ($table) {
+        Schema::create('buildingblocks', function ($table) {
             $table->increments('id');
             $table->string('type');
             $table->string('buildable_type');
             $table->integer('buildable_id')->unsigned();
-            $table->mediumText('content')->nullable->default(null);
+            $table->mediumText('content')->nullable()->default(null);
             $table->integer('order')->unsigned()->default(0);
             $table->timestamps();
         });
 
-        Schema::table('listitems', function ($table) {
+        Schema::create('listitems', function ($table) {
             $table->integer('id');
             $table->string('text');
             $table->integer('buildingblock_id')->unsigned();
--- src/Buildable.php
+++ src/Buildable.php
@@ -1,6 +1,9 @@
 <?php
 
-namespace RealRipley\LaravelSimplePageBuilder;
+namespace RealRipley\Buildable;
+
+use RealRipley\Buildable\Models\BuildingBlock;
+use RealRipley\Buildable\Models\ListItem;
 
 trait Buildable
 {
@@ -9,17 +12,47 @@ trait Buildable
 	 *
 	 * @return void
 	 */
-	public static function bootLaravelSimplePageBuilder()
+	public static function bootBuildable()
 	{
-		if(static::removeContentOnDelete()) {
-			static::deleting(function($model) {
-				$model->removeContent();
-			});
-		}
-    }
+		// if(static::removeContentOnDelete()) {
+		// 	static::deleting(function($model) {
+		// 		$model->removeContent();
+		// 	});
+		// }
+	}
+	
+	public function getContents()
+	{
+		return $this->morphMany(BuildingBlock::class, 'buildable')->orderBy('order')->get();
+	}
     
-    public function addContent()
+    public function addContentParagraph($paragraph, $order = null)
     {
+		$block = new BuildingBlock();
+		$block->type = 'paragraph';
+		$block->content = $paragraph;
+		$block->buildable_type = get_class($this);
+		$block->buildable_id = $this->id;
 
-    }
+		if ($order != null) { 
+			$block->order = $order;
+		}
+
+		$block->save();
+	}
+	
+	public function addContentImage($url, $order = null)
+    {
+		$block = new BuildingBlock();
+		$block->type = 'image';
+		$block->content = $url;
+		$block->buildable_type = get_class($this);
+		$block->buildable_id = $this->id;
+
+		if ($order != null) { 
+			$block->order = $order;
+		}
+
+		$block->save();
+	}
 }
